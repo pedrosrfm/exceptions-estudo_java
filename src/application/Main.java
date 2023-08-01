0d5273ffb0f199f8ebb23180b54f79188d8db29f
Pedro <pedrotaraves@gmail.com>
2023-08-01 14:39:09 -0300

    Second commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,48 @@
 package application;
 
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            System.out.print("Room number: ");
+            int roomNumber = sc.nextInt();
+            System.out.print("Check-in date: ");
+            Date checkin = sdf.parse(sc.next());
+            System.out.print("Check-out date: ");
+            Date checkout = sdf.parse(sc.next());
+
+            Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+            System.out.print("Reservation: " + reservation);
+
+            System.out.print("\n\nEnter date to update the reservation:\n");
+            System.out.print("Check-in date: ");
+            Date updatedCheckin = sdf.parse(sc.next());
+            System.out.print("Check-out date: ");
+            Date updatedCheckout = sdf.parse(sc.next());
+
+            reservation.updateDates(updatedCheckin, updatedCheckout);
+            System.out.print("Reservation: " + reservation);
+
+        } catch (ParseException e) {
+            System.out.print("Invalid date format");
+        } catch (DomainException e) {
+            System.out.print("Error in reservation: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.print("Unexpected error");
+        }
+
+        sc.close();
 
     }
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -13,6 +15,10 @@ public class Reservation {
 
     //construtor
     public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        if (!checkout.after(checkin))
+        {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkin = checkin;
         this.checkout = checkout;
@@ -42,6 +48,15 @@ public class Reservation {
     }
 
     public void updateDates(Date checkin, Date checkout){
+        Date now = new Date();
+        if (checkin.before(getCheckin()) || checkout.before(getCheckin()))
+        {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+        if (!checkout.after(checkin))
+        {
+            throw new DomainException("Check-out dates must be after check-in date");
+        }
         this.checkin = checkin;
         this.checkout = checkout;
     }
